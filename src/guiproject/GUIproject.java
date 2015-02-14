57bd0d3db58dcf7f49a6c030efb509206e6a49c0
Jversen <johan.iversen@gmail.com>
2015-02-14 10:55:40 +0100

    uploaded project

---
+++ src/guiproject/GUIproject.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guiproject;
+
+/**
+ *
+ * @author Johan
+ */
+public class GUIproject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        iMatFrame app = new iMatFrame();
+        app.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
